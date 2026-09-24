fb241f9320aa81fdd625b138fda063d2ce56acf1
EfraimRay <efraimrayinfo@gmail.com>
2026-09-24 19:27:04 +0800

    Add favicon and floating back-to-top button
    Added a coffee-themed favicon.ico and a floating back-to-top button with smooth scrolling.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
+let backToTop = document.querySelector('.back-to-top');
 
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
@@ -9,12 +10,20 @@ menu.onclick = () => {
 window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+    backToTop.classList.toggle('show', window.scrollY > 400);
 };
 
-document.querySelectorAll('.image-slider img').forEach(images => {
-    images.onclick = () => {
-        var src = images.getAttribute('src');
+backToTop.onclick = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+
+document.querySelectorAll('.image-slider button').forEach((thumbnail, index) => {
+    thumbnail.onclick = () => {
+        var src = thumbnail.querySelector('img').getAttribute('src');
         document.querySelector('.main-home-image').src = src;
+        document.querySelector('.hero-image-caption strong').textContent = `0${index + 1}`;
+        document.querySelector('.image-slider button.active').classList.remove('active');
+        thumbnail.classList.add('active');
     };
 });
 
